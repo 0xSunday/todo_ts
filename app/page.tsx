461f609cf50c3316df5ade9478f6084a428f1683
Sunil Reddy <sunday7637@gmail.com>
2023-09-21 10:29:23 +0530

    final

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,11 @@
-"use client";
-import React, { useState } from "react";
+import React from "react";
 
-import InputForm from "@/components/InputForm";
-import { Todo } from "@/components/Modal";
 import TodoList from "@/components/TodoList";
-const Home: React.FC = () => {
-  const [todo, setTodo] = useState<string>("");
-  const [todos, setTodos] = useState<Todo[]>([]);
-
-  const handleClick = (e: React.FormEvent) => {
-    e.preventDefault();
-    if (todo) {
-      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
-      setTodo("");
-    }
-    console.log(todos);
-  };
-
+const Home = () => {
   return (
     <div className="flex text-yellow-400 h-[100vh] bg-zinc-950 flex-col items-center px-10 py-10">
       <h1 className="text-5xl font-bold py-10">ADD YOUR TASKS</h1>
-      <InputForm todo={todo} setTodo={setTodo} handleClick={handleClick} />
-      <TodoList todos={todos} setTodos={setTodos} />
+      <TodoList />
     </div>
   );
 };
--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,17 +1,14 @@
+"use client";
 import React from "react";
-import { Todo } from "./Modal";
-interface Props {
-  todos: Todo[];
-  setTodos: (value: React.SetStateAction<Todo[]>) => void;
-}
-const TodoList: React.FC<Props> = ({ todos, setTodos }) => {
-  return (
-    <ul className="flex justify-start text-start align-baseline flex-col gap-4 py-10">
-      {todos.map((t) => (
-        <li className="text-3xl">{t.todo}</li>
-      ))}
-    </ul>
-  );
+
+const TodoList = () => {
+  const makeACall = async () => {
+    await fetch("/api/register", {
+      method: "POST",
+      body: JSON.stringify({ hello: "world" }),
+    });
+  };
+  return <button onClick={makeACall}>make a call</button>;
 };
 
 export default TodoList;
